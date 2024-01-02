0b7520dbba343c49a5b750592267d5be21336c81
andtr-2021 <an.dtruongan@gmail.com>
2024-01-02 17:32:29 +0700

    done with app.py

--- fred-api/app.py
+++ fred-api/app.py
@@ -6,22 +6,120 @@
 """
 
 from taipy.gui import Gui
+import requests
+import pandas as pd
 
+# SCRAPE data from FRED when the app is loaded
+api_key = '10644614d71d570b8fb28c49378b7005'
+base_url = 'https://api.stlouisfed.org/fred/'
+obs_endpoint = 'series/observations'
+
+# Assign parameters
+series_id = 'GDP'
+start_date = '2000-01-01'
+end_date = '2023-07-01'
+ts_frequency = 'q'
+
+obs_params = {
+    'series_id': series_id,
+    'api_key': api_key,
+    'file_type': 'json',
+    'observation_start': start_date,
+    'observation_end': end_date,
+    'frequency': ts_frequency
+}
+
+response = requests.get(base_url + obs_endpoint, params=obs_params)
+
+if response.status_code == 200:
+    res_data = response.json()
+    obs_data = pd.DataFrame(res_data['observations'])
+    obs_data['date'] = pd.to_datetime(obs_data['date'])
+    if obs_data['value'].dtype == object:
+        try :
+            obs_data['value'] = obs_data['value'].astype(float)
+        except ValueError:
+            print('Failed to convert to float')
+    elif obs_data['value'].dtype == float:
+        pass
+else:
+    print('Failed to retrieve data. Status code:', response.status_code)
+
+# create a pandas dataframe
+print("Initial Data:")
+print(obs_data.head())
+# create a dict for chart data
+chart_data = { "Date": obs_data['date'], "Values": obs_data['value'] }
+
+# filter variables
 startdate = "2000-01-01"
 enddate = "2023-07-01"
-rangeselector = ["quarterly", "semiannual", "annual"]
-daterange = "quarterly"
+frequency_list = ["Daily", "Weekly", "Monthly", "Quarterly", "Semiannual", "Annual"]
+chosen_frequency = "Quarterly"
 
-data = {"x_col":[0,1,2], "y_col1":[4,1,2]}
+# Plot multiple series on a same chart 
+series_list = ["CPIAUCSL", "NIKKEI225", "PCU2122212122210", "APU0000708111"]
+
+final_data = pd.DataFrame()
+
+a = 1 
+
+# collect data 
+for i in series_list:
+    series_id = i 
+    start_date = '2000-01-01'
+    end_date = '2023-07-01'
+    ts_frequency = 'q'
+
+    obs_params = {
+        'series_id': series_id,
+        'api_key': api_key,
+        'file_type': 'json',
+        'observation_start': start_date,
+        'observation_end': end_date,
+        'frequency': ts_frequency
+    }
+
+    response = requests.get(base_url + obs_endpoint, params=obs_params)
+
+    if response.status_code == 200:
+        res_data = response.json()
+        obs_data = pd.DataFrame(res_data['observations'])
+        obs_data['date'] = pd.to_datetime(obs_data['date'])
+        if obs_data['value'].dtype == object:
+            try :
+                obs_data['value'] = obs_data['value'].astype(float)
+            except ValueError:
+                print('Failed to convert to float')
+        elif obs_data['value'].dtype == float:
+            pass
+    else:
+        print('Failed to retrieve data. Status code:', response.status_code)
+
+    # create a pandas dataframe
+    print("Initial Data:")
+    print(obs_data.head())
+    # create a dict for chart data
+    chart_data = { "Date": obs_data['date'], "Values": obs_data['value'] }
+
+    if a == 1:
+        # get the date columns 
+        final_data['Date'] = obs_data['date']
+        final_data[i] = obs_data['value']
+        a += 1 
+    else: 
+        # get the value columns 
+        final_data[i] = obs_data['value']
+
+print(final_data.head())    
+
+multiple_line_chart_date = { "Date": final_data['Date'], "CPIAUCSL": final_data['CPIAUCSL'], "NIKKEI225": final_data['NIKKEI225'] }
 
 page = """
 
 <center><h1> FRED Macro Economic Data </h1></center>
-
 <|layout|columns=50px 300px 1000px|
-
 <|layout|columns= 1|
-
 |>
 
 <|layout|columns= 1|
@@ -33,39 +131,25 @@ Date Start:
 Date End:
 <|{enddate}|date|>
 <br></br>
-Date Range: <|{daterange}|>
-<|{daterange}|selector|lov={rangeselector}|>
+Frequency Ranges: <|{chosen_frequency}|>
+<|{chosen_frequency}|selector|lov={frequency_list}|dropdown|>
 
 <|Apply|button|on_action=data_collection|>
 |>
 
 <|layout|columns= 1|
-<h2> Gross Domestic Product (GDP) </h2>
-<|{data}|chart|x=x_col|y=y_col1|>>
+<h2> GDP Chart </h2>
+<|{chart_data}|chart|mode=lines|x=Date|y=Values|>
+
+<br></br>
+
+<|{final_data}|chart|mode=lines|x=Date|y[1]=CPIAUCSL|y[2]=PCU2122212122210|y[3]=APU0000708111|>
 |>
 |>
 
 """
 
-import requests
-import pandas as pd
-
-api_key = '10644614d71d570b8fb28c49378b7005'
-
-# Define the FRED API endpoint
-base_url = 'https://api.stlouisfed.org/fred/'
-
-# Assign endpoint
-obs_endpoint = 'series/observations'
-
-
-
 def data_collection(state):
-    print(state.startdate)
-    print(state.enddate)
-    print(state.daterange)
-
-    # Assign parameters
     series_id = 'GDP'
     start_date = state.startdate
     end_date = state.enddate
@@ -84,24 +168,23 @@ def data_collection(state):
 
     if response.status_code == 200:
         res_data = response.json()
-        print(res_data.keys())
         obs_data = pd.DataFrame(res_data['observations'])
         obs_data['date'] = pd.to_datetime(obs_data['date'])
-        obs_data.set_index('date', inplace=True)
         if obs_data['value'].dtype == object:
-            print('Converting to numeric')
+            try :
+                obs_data['value'] = obs_data['value'].astype(float)
+            except ValueError:
+                print('Failed to convert to float')
         elif obs_data['value'].dtype == float:
             pass
     else:
         print('Failed to retrieve data. Status code:', response.status_code)
 
+    # create a pandas dataframe
+    print("Updated Data:")
     print(obs_data.head())
-
-    state.date.x_col = obs_data['date']
-    state.date.y_col1 = obs_data['value']
-
-    print(state.date.x_col)
-    print(state.date.y_col1)
+    # create a dict for chart data
+    state.chart_data = { "Date": obs_data['date'], "Values": obs_data['value'] }
 
 Gui(page).run(dark_mode=False)
 
